7f5f70ed5f00c77ecbf8225979a8cf0b828810d1
aurelienniederhauser <aurelien.niederha@hes-so.ch>
2026-09-22 10:40:13 +0200

    fix ex1

--- exercices_progweb2/1_InitiationAuxFonctions.js
+++ exercices_progweb2/1_InitiationAuxFonctions.js
@@ -1,20 +1,19 @@
 // 1 - Ecrire une fonction qui retourne la plus grande valeur parmi les trois nombres fournis en paramètre.
 console.log("exercice 1");
 function compare(a, b, c){
-
-    if (a > b){
-        if (a > c){
-            console.log(a);
-        } else {
-            console.log(c);
+        if (a > b){
+            if (a > c){
+                console.log(a);
+            } else {
+                console.log(c);
+            }
+        } else if (a < b) {
+            if (b > c){
+                console.log(b);
+            } else {
+                console.log(c);
+            }
         }
-    } else if (a < b) {
-        if (b > c){
-            console.log(b);
-        } else {
-            console.log(c);
-        }
-    }
 }
 
 const a = 4;
@@ -80,6 +79,17 @@ function number(n) {
         }
     }
     console.log("Les nombres entiers pairs et multiples de 3, ainsi que les nombres entiers multiple de 7 compris entre 0 et n.");
+    for (let i=0; i<=n; i++){
+        if(i % 2 == 0) {
+            if(i % 3 == 0) {
+                console.log(i);
+            }
+        }
+        if(i % 7 == 0) {
+            console.log(i);
+        }
+    }
+    console.log("Les nombres entiers pairs et multiples de 3, mais non multiples de 7 compris entre 0 et n.");
     for (let i=0; i<=n; i++){
         if(i % 2 == 0) {
             if(i % 3 == 0) {
